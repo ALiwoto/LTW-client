291b521e74b52956833e7017125cc145339a3284
Artoria Pendoragon <79416579+ArthuriaDX@users.noreply.github.com>
2021-05-01 17:25:31 -0400

    Illusion Basic Methods (#12)
    * Aliwoto (#11)
    
    * SERVER_TEST
    
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>
    
    * fixed problems in Illusion.cs
    
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>
    
    * added Illision methods
    
    Signed-off-by: ArthuriaDX <79416579+ArthuriaDX@users.noreply.github.com>
    
    * cleaning up
    
    Signed-off-by: ArthuriaDX <79416579+ArthuriaDX@users.noreply.github.com>
    
    Co-authored-by: ALi.w <70187458+ALiwoto@users.noreply.github.com>

--- LTW/Client/GameClient.cs
+++ LTW/Client/GameClient.cs
@@ -136,6 +136,7 @@ namespace LTW.Client
             MainMenuLoaded              = false;
             ReleasingDate               = null;
             //---------------------------------------------
+
         }
         #endregion
         //-------------------------------------------------
