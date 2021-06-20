92949acc04295b9fc82de826b1b343ef6f8d5fb4
ALi.w <73873634+RokuroBenio@users.noreply.github.com>
2021-06-20 17:31:34 +0000

    Updated run.sh file

--- run.sh
+++ run.sh
@@ -3,15 +3,19 @@
 # This file is subject to the terms and conditions defined in
 # file 'LICENSE', which is part of the source code.
 
+# clear the screen (the terminal)
 clear
 
 echo -e "bulding it, please wait a bit..."
 
+# build the solution (*.sln file)
 dotnet build
 
+# clear the screen (the terminal)
 clear
 
 echo -e "we are done building it,\n now running the game...\n-------------------"
 
-
+# finally, run the executable file which is in debug directory
 LTW/bin/Debug/net5.0/linux-x64/LTW
+
